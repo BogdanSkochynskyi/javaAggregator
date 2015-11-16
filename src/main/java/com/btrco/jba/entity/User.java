be77acb86c25fc5ae0fee3e812bbb751568da7f9
bogdans <bogdanskochynskyi@gmail.com>
2015-11-16 15:21:38 +0200

    [S.W.A.T.  9] Add new entities and relations between them

--- src/main/java/com/btrco/jba/entity/User.java
+++ src/main/java/com/btrco/jba/entity/User.java
@@ -1,9 +1,8 @@
 package com.btrco.jba.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class User {
@@ -12,11 +11,56 @@ public class User {
     @GeneratedValue
     private Integer id;
 
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany
+    @JoinTable
+    private List<Role> roles;
+
+    @OneToMany(mappedBy = "user")
+    private List<Blog> blogs;
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String passeord) {
+        this.password = passeord;
     }
 }
